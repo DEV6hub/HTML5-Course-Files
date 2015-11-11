2ee5cbbf12ad419ddd6efd0801352c498fd7d957
Iva Delic <ms.iva.delic@gmail.com>
2015-11-11 15:13:14 -0500

    Added Powerpoint presentations and finishing off Unit09 CSS

--- Demos/Unit09-CSS3/scripts/3DTransforms-Perspective.js
+++ Demos/Unit09-CSS3/scripts/3DTransforms-Perspective.js
@@ -1,14 +1,14 @@
 window.addEventListener('load', function () {
-    document.getElementById('perspective').addEventListener('change', updateParentPerspective);
-    document.getElementById('reset').addEventListener('change', doReset);
-  });
+  document.getElementById('perspective').addEventListener('change', updateParentPerspective);
+  document.getElementById('reset').addEventListener('change', doReset);
+});
 
-  function updateParentPerspective() {
-    var targetValue = document.getElementById('perspective').value;
-    document.getElementById('perspectiveOD').innerHTML = targetValue;
-    document.getElementById(event.target.id+'OD').innerHTML = targetValue;
-    document.getElementById('parentBox').style['webkitPerspective']=targetValue+'px';
-  }
-  function doReset() {
-    document.getElementById('parentBox').style['webkitPerspective']='';
-  }
+function updateParentPerspective() {
+  var targetValue = document.getElementById('perspective').value;
+  document.getElementById('perspectiveOD').innerHTML = targetValue;
+  document.getElementById(event.target.id+'OD').innerHTML = targetValue;
+  document.getElementById('parentBox').style['webkitPerspective']=targetValue+'px';
+}
+function doReset() {
+  document.getElementById('parentBox').style['webkitPerspective']='';
+}
