a5672d79d7fdcf36b0371d728997ff8231e7f811
Iva Delic <ivad@newyyz.com>
2015-11-16 08:45:22 -0500

    Added in geolocation

--- Demos/Unit08-APIs/scripts/weather.js
+++ Demos/Unit08-APIs/scripts/weather.js
@@ -8,10 +8,10 @@ function getWeather(){
       var xDoc = JSON.parse(xhr.responseText);
       var weather = JSON.stringify(xDoc.weather);
       document.getElementById("weather").innerHTML = JSON.stringify(xDoc.weather[0].main);
-      // var temp = xDoc.main.temp;
-      // var celsius = temp - 273.15;
-      // document.getElementById("temp").innerHTML = Math.round(celsius) + "&#176;" + "C";
-      // console.log(xDoc);
+      var temp = xDoc.main.temp;
+      var celsius = temp - 273.15;
+      document.getElementById("temp").innerHTML = Math.round(celsius) + "&#176;" + "C";
+      console.log(xDoc);
     }
   };
   xhr.open("GET", "http://api.openweathermap.org/data/2.5/weather?q=Toronto,on&appid=2de143494c0b295cca9337e1e96b00e0");
